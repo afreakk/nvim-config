e74547a44508ef222a3c07bfdc4472b31869bcd4
afreakk <laderud@hotmail.com>
2022-12-18 10:56:10 +0100

    fix qftoggle

--- lua/afreak/qftoggle.lua
+++ lua/afreak/qftoggle.lua
@@ -1,23 +1,13 @@
 local M = {}
-local qfisopen = 0
-function M.toggle()
-    if qfisopen == 0 then
-        vim.cmd.copen()
-    else
-        vim.cmd.cclose()
+
+M.toggle = function()
+    for _, win in pairs(vim.fn.getwininfo()) do
+        if win['quickfix'] == 1 then
+            vim.cmd.cclose()
+            return
+        end
     end
+    vim.cmd.copen()
 end
 
-vim.api.nvim_create_autocmd(
-    "BufWinEnter",
-    { pattern = { "quickfix" }, callback = function()
-        qfisopen = 1
-    end }
-)
-vim.api.nvim_create_autocmd(
-    "BufWinLeave",
-    { pattern = { "*" }, callback = function()
-        qfisopen = 0
-    end }
-)
 return M
