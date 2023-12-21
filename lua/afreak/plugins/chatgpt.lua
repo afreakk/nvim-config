d26b253199c670157b983c49d6388b4ac4903d81
afreakk <laderud@hotmail.com>
2023-12-21 19:39:24 +0100

    yup

--- lua/afreak/plugins/chatgpt.lua
+++ lua/afreak/plugins/chatgpt.lua
@@ -9,6 +9,7 @@ return {
         event = "VeryLazy",
         config = function()
             require("chatgpt").setup({
+                -- todo something different for this
                 api_key_cmd = "cat /home/afreak/random/openai_api_key",
                 openai_params = {
                     model = "gpt-4",
